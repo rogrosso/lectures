e0b95dd179181e7f03cc5776015cd86f6acb84da
rogrosso <roberto.grosso@fau.de>
2023-03-03 17:08:23 +0100

    Add transfer functions
    Add transfer functions and make some code improvement, such as some issues with the continuous color scales. Some functions were moved to the common folder for resuse.

---
+++ infovis/colors/transferfunctions.js
@@ -0,0 +1,244 @@
+import { axes, conScale, transferFunctions } from "draw"
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
+
+let divTooltip = d3.select("body")
+    .append('div')
+    .attr('class', 'categorical_tooltip')
+    .attr('id', 'catcolortooltip')
+    .style("opacity", 0.7)
+    .style('display', 'none')
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+// canvas
+let colSel = 'BrBG'
+const colId = 'color-scale'
+const gridObj = d3.select('#transfer-functions')
+const guiDiv = gridObj.append('div')
+        .attr('class', 'cell')
+        .attr('id', colId)
+
+const width = 450
+const sclWidth = width
+const sclHeight = 60
+const sclMargin = { top: 5, bottom: 5, left: 30, right: 10}
+const sclW = sclWidth - sclMargin.left - sclMargin.right
+const sclH = sclHeight - sclMargin.top - sclMargin.bottom
+const sclDiv = gridObj.append('div')
+    .attr('class', 'cell')
+    .attr('id', colId)
+const sclSVG = sclDiv
+    .append('svg')
+    .attr('class', colId)
+    .attr('width', sclWidth)
+    .attr('height', sclHeight)
+const sclG = sclSVG.append('g')
+    .attr('transform',`translate(${sclMargin.left}, ${sclMargin.top})`)
+    .attr('class','color-scale-group')
+
+const trfWidth = width
+const trfHeight = 250
+const trfMargin = { top: 15, bottom: 30, left: 30, right: 10}
+const trfW = trfWidth - trfMargin.left - trfMargin.right
+const trfH = trfHeight - trfMargin.top - trfMargin.bottom
+const trfDiv = gridObj.append('div')
+        .attr('class', 'cell')
+        .attr('id', colId)
+const trfSVG = trfDiv
+    .append('svg')
+    .attr('class', colId)
+    .attr('width', trfWidth)
+    .attr('height', trfHeight)
+const axisG = trfSVG.append('g')
+    .attr('transform',`translate(${trfMargin.left}, ${trfMargin.top})`)
+    .attr('class','color-scale-axis-group')
+const trfG = trfSVG.append('g')
+    .attr('transform',`translate(${trfMargin.left}, ${trfMargin.top})`)
+    .attr('class','transfer-function-group')
+
+    // color scales
+const colMap = new Map()
+const colKeys = ["BrBG","RdBu","Spectral","Blues","Viridis","Magma","Plasma","Warm","Cool","Cubehelix","Rainbow","Sinebow"]
+const colDefs = [
+    { name: "interpolateBrBG" },
+    { name: "interpolateRdBu" },
+    { name: "interpolateSpectral" },
+    { name: "interpolateBlues"    },
+    { name: "interpolateViridis"  },
+    { name: "interpolateMagma"    },
+    { name: "interpolatePlasma"   },
+    { name: "interpolateWarm"     },
+    { name: "interpolateCool"     },
+    { name: "interpolateCubehelixDefault" },
+    { name: "interpolateRainbow" },
+    { name: "interpolateSinebow" }
+]
+for(let i = 0; i < colKeys.length; i++) {
+    colMap.set(colKeys[i], colDefs[i])
+}
+
+// draw menue
+const guiConfig = {
+    divObj: guiDiv,
+    text: 'color scale: ',
+    selection: colSel,
+    keys: colMap.keys(),
+    handler: colHandler
+}
+dropdown(guiConfig)
+
+// color scale
+conScale(sclG, colId, colSel, colMap, sclW, sclH)
+
+// add svg and append axes
+const xAxisScale = d3.scaleLinear()
+    .domain([0,1])
+    .range([0,trfW])
+const yAxisScale = d3.scaleLinear()
+    .domain([0,255])
+    .range([trfH,0])
+const yTickValues = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 255]
+const axisConfig = {
+    selection: axisG,
+    width: trfW,
+    height: trfH,
+    xScale: xAxisScale,
+    yScale: yAxisScale,
+    xPos: 0,
+    yPos: 0,
+    className: 'color-scale-axis',
+    xTickValues: undefined,
+    yTickValues: yTickValues,
+    xTickSize: undefined,
+    yTickSize: undefined,
+    xTickFormat: undefined,
+    yTickFormat: undefined,
+    xLabel: undefined,
+    yLabel: undefined
+}
+axes(axisConfig)
+// draw transfer functions
+transferFunctions(trfG, { colSel: colSel, colMap: colMap, xScale: xAxisScale, yScale: yAxisScale })
+
+function colHandler(text, value) {
+    console.log(value)
+    transferFunctions(trfG, { colSel: value, colMap: colMap, xScale: xAxisScale, yScale: yAxisScale })
+    conScale(sclG, colId, value, colMap, sclW, sclH)
+}
+
+function dropdown({
+    divObj,
+    text,
+    selection,
+    keys,
+    handler
+}) {
+    const dropdownOptions = []
+    for (let k of keys) dropdownOptions.push({key: k, value: k})
+
+    const lab = divObj.append('label')
+        .text(text)
+    const select = divObj.append('select')
+        .on('change', function(event) {
+            handler(text, event.target.value)
+        })
+    const options = select.selectAll('option').data(dropdownOptions, d => d.key)
+        .join('option')
+        .attr('value', d => d.value)
+        .property('selected', d => d.key === selection)
+        .text(d => d.value)
+}
+
+function mouseOver() {
+    divTooltip
+        .style('position', 'absolute')
+        .style('display', 'inline-block')
+}
+function mouseMove(hexCol, rgbCol, pos) {
+    const {
+        x,
+        y
+    } = pos
+        divTooltip
+            .html('color: ' + hexCol + ', ' + rgbCol)
+            .style('left', `${x + 10}px`)
+            .style('top', `${y}px`)
+}
+function mouseOut() {
+    divTooltip
+    .style("display", 'none')
+}
+
+const cText = `
+// plot transfer functions
+function transferFunctions (selection, props) {
+    const {
+        colSel,
+        colMap,
+        xScale,
+        yScale
+    } = props
+    selection.selectAll("*").remove()    
+    const id = 'cont-color-scale'
+    const nrColors = 100
+    const colorScale = d3.scaleSequential()
+        .domain([0, nrColors])
+        .interpolator(d3[colMap.get(colSel).name])
+    // collect points
+    const points = []
+    const cstep = nrColors / (nrColors - 1)
+    let c = 0
+    for (let i = 0; i < nrColors; i++) {
+        const color = d3.color(colorScale(c))
+            points.push({
+                x: c / nrColors,
+                r: color.r / 255,
+                g: color.g / 255,
+                b: color.b / 255
+            })
+            c += cstep
+    }
+    function colPath(sel, points, color, c, duration) {
+        // Path
+        const gPath = sel.append('g')
+        gPath
+            .attr('class', 'path-container')
+        const cPath = gPath.append('path')
+            .datum(points)
+            .attr('class', color + '-path')
+            .attr('stroke', color)
+            .attr('stroke-width', 1.5)
+            .attr('fill', 'none')
+            .attr('d', d3.line()
+                .curve(d3.curveBasis)
+                .x(function (d) {
+                    return xScale(d.x)
+                })
+                .y(function (d) {
+                    return yScale(255 * d[c])
+                }))
+            .transition().duration(duration)
+            .delay((d, i) => i * 5)
+            .attrTween('stroke-dasharray', function () {
+                const len = this.getTotalLength();
+                return function (t) {
+                    return (d3.interpolateString("0," + len, len + ",0"))(t)
+                }
+            })
+        return gPath
+    }
+    const duration = 1000
+    colPath(selection, points, 'red', 'r', duration)
+    colPath(selection, points, 'green', 'g', duration)
+    colPath(selection, points, 'blue', 'b', duration)
+    return selection
+}
+`
+const hlPre = d3.select('#hl-code').append('pre')
+const hlCod = hlPre.append('code')
+    .attr('class', 'language-javascript')
+    .attr('style', 'border: 1px solid #C1BAA9')
+    .text(cText)
--- infovis/common/draw.js
+++ infovis/common/draw.js
@@ -33,6 +33,8 @@ export function axes(config) {
     const xAxis = d3.axisBottom(xScale)
     if (xTickSize !== undefined) {
         xAxis.tickSize(xTickSize)
+    } else {
+        xAxis.tickSize(-height)
     }
     if (xTickValues !== undefined) {
         xAxis.tickValues(xTickValues)
@@ -44,8 +46,11 @@ export function axes(config) {
         .attr('transform', `translate(0, ${height})`)
     
     const yAxis = d3.axisLeft(yScale)
-    if (yTickSize !== undefined)
+    if (yTickSize !== undefined) {
+        yAxis.tickSize(-yTickSize)
+    } else {
         yAxis.tickSize(-width)
+    }
     if (yTickValues !== undefined) {
         yAxis.tickValues(yTickValues)
     }
@@ -112,3 +117,215 @@ export const fetchCSV = async (url) => {
 }
 
 
+
+
+export function conScale(selection, scaleId, scale, map, width, height) {
+    selection.selectAll('*').remove()
+    const maxIndex = 30
+    const xScale = d3.scaleLinear().domain([0, maxIndex]).range([0,100])
+    const cScale = d3.scaleLinear().domain([0,width]).range([0,100])
+    const colorScale = d3.scaleSequential().domain([0,100]).interpolator(d3[map.get(scale).name])
+    const containerClass = 'g.'+scaleId
+    const gContainer = selection.selectAll(containerClass).data([null])
+        .join(
+            function(enter) {
+                return enter.append('g')
+                    .attr('class', 'colors.'+scaleId)
+            },
+            function(update) {
+                return update
+            },
+            function(exit) {
+                return exit
+            }
+        )
+    const dContainer = gContainer.selectAll('defs').data([null]).join(function(enter) { return enter.append('defs') } )
+    const lContainer = dContainer.selectAll('defs').data([null])
+        .join(
+            function(enter) {
+                return enter.append('linearGradient').attr('id', 'linear-gradient.'+scaleId)
+            }
+        )
+    const sContainer = lContainer.selectAll('stops')
+        .data(d3.range(maxIndex+1), d => colorScale(d))
+        .join(
+            function(enter) {
+                return enter
+                    .append('stop')
+                    .attr('offset', d => xScale(d) + '%')
+                    .attr('stop-color', d => colorScale(xScale(d)))
+            },
+            function(update) { 
+                return update.attr('stop-color', d => colorScale(d))
+            },
+            function(exit) {
+                return exit
+                    .transition()
+                    .duration(100)
+                    .attr('width',0)
+                    .remove()
+            }
+        )    
+    const colors = gContainer.selectAll('rect').data([null])
+        .join(
+            function(enter) {
+                return enter
+                .append('rect')
+                .attr('class', 'rect')
+                .attr('y', 0)
+                .attr('x', 0)
+                .attr('width', 0)
+                .attr('height', 0)
+                .on('mouseover', function (event, d) {
+                    mouseOver()
+                })
+                .on('mousemove', function (event, d) {
+                    const pos = d3.pointer(event)
+                    const rgbColor = colorScale(cScale(pos[0]))
+                    const hexColor = d3.color(rgbColor).formatHex()
+                        mouseMove(hexColor, rgbColor, {
+                            x: event.x,
+                            y: event.y
+                        })
+                })
+                .on('mouseout', function (event, d) {
+                    mouseOut()
+                })
+                .transition().duration(1000)
+                .attr('width', width)
+                .attr('height', height)
+                .attr('fill', "url(#linear-gradient." + scaleId + ")")
+            }
+        )
+}
+
+export function catScale(selection, scale, map, width, height) {
+    const nrColors = map.get(scale).n    
+    const dx = width / nrColors
+    const dy = height
+    const xScale = d3.scaleLinear()
+        .domain([0,nrColors])
+        .range([0,width])
+
+    const gContainer = selection.selectAll('g.catContainer').data([null])
+        .join(
+            function(enter) {
+                enter.append('g')
+                    .attr('class', 'catContainer')
+                return enter
+            },
+            function(update) {
+                return update
+            },
+            function(exit) {
+                return exit
+            }
+        )
+    const cGroup = d3.select('g.catContainer')
+        .selectAll('rect')
+        .data(d3[map.get(scale).name], d => d)
+        .join(
+            function(enter) {
+                return enter.append('rect')
+                    .attr('y', 0)
+                    .attr('fill', d => d)
+                    .style('opacity', 0)
+                    .on('mouseover', function (event, d) {
+                        mouseOver()
+                    })
+                    .on('mousemove', function (event, d) {
+                        const rgbColor = d3.color(d)
+                            mouseMove(d, rgbColor, {
+                                x: event.x,
+                                y: event.y
+                            })
+                    })
+                    .on('mouseout', function (event, d) {
+                        mouseOut()
+                    })
+                    .transition().duration(1000)
+                    .delay((d, i) => i * 5)
+                    .attr('x', (d, i) => xScale(i))
+                    .attr('width', dx)
+                    .attr('height', dy)
+                    .style('opacity', 1)
+                    
+            },
+            function(update) {
+                return update
+                    .transition().duration(200)
+                    .attr('width', 0)
+                    .attr('x', 0)
+            },
+            function(exit) {
+                return exit
+                    .transition().duration(200)
+                    .attr('width', 0)
+                    .attr('height', 0)
+                    .attr('x', 0)
+                    .remove()
+            }
+        )
+}
+export function transferFunctions (selection, props) {
+    const {
+        colSel,
+        colMap,
+        xScale,
+        yScale
+    } = props
+    selection.selectAll("*").remove()    
+    const id = 'cont-color-scale'
+    const nrColors = 100
+    const colorScale = d3.scaleSequential()
+        .domain([0, nrColors])
+        .interpolator(d3[colMap.get(colSel).name])
+    // collect points
+    const points = []
+    const cstep = nrColors / (nrColors - 1)
+    let c = 0
+    for (let i = 0; i < nrColors; i++) {
+        const color = d3.color(colorScale(c))
+            points.push({
+                x: c / nrColors,
+                r: color.r / 255,
+                g: color.g / 255,
+                b: color.b / 255
+            })
+            c += cstep
+    }
+    function colPath(sel, points, color, c, duration) {
+        // Path
+        const gPath = sel.append('g')
+        gPath
+            .attr('class', 'path-container')
+        const cPath = gPath.append('path')
+            .datum(points)
+            .attr('class', color + '-path')
+            .attr('stroke', color)
+            .attr('stroke-width', 1.5)
+            .attr('fill', 'none')
+            .attr('d', d3.line()
+                .curve(d3.curveBasis)
+                .x(function (d) {
+                    return xScale(d.x)
+                })
+                .y(function (d) {
+                    return yScale(255 * d[c])
+                }))
+            .transition().duration(duration)
+            .delay((d, i) => i * 5)
+            .attrTween('stroke-dasharray', function () {
+                const len = this.getTotalLength();
+                return function (t) {
+                    return (d3.interpolateString("0," + len, len + ",0"))(t)
+                }
+            })
+        return gPath
+    }
+    const duration = 1000
+    colPath(selection, points, 'red', 'r', duration)
+    colPath(selection, points, 'green', 'g', duration)
+    colPath(selection, points, 'blue', 'b', duration)
+    return selection
+}
