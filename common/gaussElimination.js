e6277f6f0c68fa26a4251ec74954a4ccd00efc86
rogrosso <roberto.grosso@fau.de>
2023-04-21 11:21:42 +0200

    Add numerical software
    Add and replace code for solving linear algebra problems. Avoid license problems by implementing the code following text books.

--- common/gaussElimination.js
+++ common/gaussElimination.js
@@ -1,146 +1,140 @@
-/* Gauss-Jordan Elimination
- * This is a modified version of an implementation I found in the internet many years ago, 
- * but I do not remember where. The code does pretty much the same processing steps that 
- * you learn in a text book, e.g. the Wikipedia article Gaussian elimination.
- * The implementation uses pivoting, and it is able to handle two dimensional arrays for 
- * the rhs, which might might be useful for solving multiple linear systems at once, which 
- * have the same system matrix but different right hand sides.
+/**
+ * Gauss Elimination
+ * The implementation is based on the text book 
+ * Hans Rudolf Schwarz I Norbert Köckler 
+ * Numerische Mathematik 
+ * 7., überarbeitete Auflage, 2009
  */
-export default function gaussEliminationFactory() {
-    let n = 0
+export default function gFactory() {
+    let n = 0      // matrix size
     let pIndex = 0 // pivot index
-    let sign = 1 // sign to account for rows swap
+    let sign = 1   // sign to account for rows swap
     const swapRows = (A, i, j) => [A[i], A[j]] = [A[j], A[i]]
-    const f1 = m => {
+    function g(R, b) {
         sign = 1
-        for (let row = 0; row < n; row++) {
-            pIndex = row
-            for (let i = row + 1; i < n; i++) { // pivot
-                if (Math.abs(m[i][row]) > Math.abs(m[pIndex][row])) pIndex = i
+        for (let c = 0; c < n; c++) { // loop over the columns
+            pIndex = c
+            for (let i = c + 1; i < n; i++) { // pivot
+                if (Math.abs(R[i][c]) > Math.abs(R[pIndex][c])) pIndex = i
             }
-            if (Math.abs(m[row][pIndex]) < Number.EPSILON) return true // not invertible
-            if (pIndex != row) {
-                swapRows(m, row, pIndex)
+            if (Math.abs(R[pIndex][c]) < Number.EPSILON) return false // not invertible
+            if (pIndex != c) {
+                swapRows(R, c, pIndex)
+                swapRows(b, c, pIndex)
                 sign *= -1
             }
-            for (let i = row + 1; i < n; i++) {
-                const s = m[i][row] / m[row][row]
-                for (let j = row + 1; j <= n; j++) {
-                    m[i][j] -= m[row][j] * s
+            for (let i = c + 1; i < n; i++) { // loop over rows
+                const lic = R[i][c] / R[c][c]
+                for (let j = c + 1; j < n; j++) { // loop over columns
+                    R[i][j] -= R[c][j] * lic
                 }
-                m[i][row] = 0
+                b[i] -= b[c] * lic
+                R[i][c] = 0
             }
         }
-        return false
-    } // rowEchelon()
-    const s1 = (m, x) => {
-        for (let i = n - 1; i >= 0; i--) {
-            x[i] = m[i][n]
-            for (let j = i + 1; j < n; j++) {
-                x[i] -= m[i][j] * x[j]
+        return true
+    } // Gauss elimination, single equation
+    function gM(R, b) {
+        sign = 1
+        const nr_e = b[0].length
+        for (let c = 0; c < n; c++) { // loop over the columns
+            pIndex = c
+            for (let i = c + 1; i < n; i++) { // pivot
+                if (Math.abs(R[i][c]) > Math.abs(R[pIndex][c])) pIndex = i
             }
-            x[i] = x[i] / m[i][i]
+            if (Math.abs(R[pIndex][c]) < Number.EPSILON) return false // not invertible
+            if (pIndex != c) {
+                swapRows(R, c, pIndex)
+                swapRows(b, c, pIndex)
+                sign *= -1
+            }
+            for (let i = c + 1; i < n; i++) { // loop over rows
+                const lic = R[i][c] / R[c][c]
+                for (let j = c + 1; j < n; j++) { // loop over columns
+                    R[i][j] -= R[c][j] * lic
+                }
+                for (let e = 0; e < nr_e; e++) b[i][e] -= b[c][e] * lic
+                R[i][c] = 0
+            }
+        }
+        return true
+    } // Gauss elimination, multiple equations
+    function solve_(R, b, x) {
+        for (let i = n - 1; i >= 0; i--) {
+            x[i] = b[i]
+            for (let j = i + 1; j < n; j++) {
+                x[i] -= R[i][j] * x[j]
+            }
+            x[i] = x[i] / R[i][i]
         }
         return true
     } // solve()
-    const fm = (m,b) => {
-        sign = 1
-        const nr_q = b[0].length
-        for (let row = 0; row < n; row++) {
-            pIndex = row
-            for (let i = row + 1; i < n; i++) { // pivot
-                if (Math.abs(m[i][row]) > Math.abs(m[pIndex][row])) pIndex = i
-            }
-            if (Math.abs(m[row][pIndex]) < Number.EPSILON) return true // not invertible
-            if (pIndex != row) {
-                swapRows(m, row, pIndex)
-                swapRows(b, row, pIndex)
-                sign *= -1
-            }
-            for (let i = row + 1; i < n; i++) {
-                const s = m[i][row] / m[row][row]
-                for (let j = row + 1; j < n; j++) {
-                    m[i][j] -= m[row][j] * s
-                }
-                for (let e = 0; e < nr_q; e++) {
-                    b[i][e] -= b[row][e] * s
-                }
-                m[i][row] = 0
-            }
-        }
-        return false
-    }
-    const sm = (m, b, x) => {
+    function solveM_(R, b, x) {
         const nr_e = b[0].length
-        for (let i = n - 1; i >= 0; i--) {
-            for(let e = 0; e < nr_e; e++) {
+        for (let e = 0; e < nr_e; e++) {
+            for (let i = n - 1; i >= 0; i--) {
                 x[i][e] = b[i][e]
                 for (let j = i + 1; j < n; j++) {
-                    x[i][e] -= m[i][j] * x[j][e]
+                    x[i][e] -= R[i][j] * x[j][e]
                 }
-                x[i][e] = x[i][e] / m[i][i]
-            }   
+                x[i][e] = x[i][e] / R[i][i]
+            }
         }
         return true
     }
     return {
-        gaussElimination(A, b) {
+        solve(A, rhs) {
             n = A.length
-            if (Array.isArray(b[0])) {
-                const m = new Array(n).fill(0).map((e, i) => [...A[i]])
-                const nr_e = b[0].length
-                const rhs = new Array(n).fill(null).map( (e,i) => [...b[i]])
-                const singular = fm(m,rhs)
-                if (singular) return null
-                const x = new Array(n).fill(null).map( (e,i) => new Array(nr_e).fill(0))
-                sm(m, rhs, x)
-                return x
-            } else { 
-                const m = new Array(n).fill(0).map((e, i) => [...A[i], b[i]])
-                const singular = f1(m)
-                if (singular) return null
-                const x = new Array(n).fill(0)
-                s1(m, x)
-                return x
-            }
+            const R = new Array(n).fill(0).map((e, i) => [...A[i]])
+            const b = rhs.map(e => e)
+            if (!g(R,b)) return null
+            const x = new Array(n).fill(0)
+            solve_(R, b, x)
+            return x
         },
         inverse(A) {
             n = A.length
-            const idM = new Array(n).fill(null).map(e => new Array(n).fill(0))
-            for (let i = 0; i < n; i++) idM[i][i] = 1
-            return this.gaussElimination(A, idM)
+            const R = new Array(n).fill(0).map((e, i) => [...A[i]])
+            const b = new Array(n).fill(null).map(e => new Array(n).fill(0))
+            const x = new Array(n).fill(null).map(e => new Array(n).fill(0))
+            for (let i = 0; i < n; i++) b[i][i] = 1
+            if(!gM(R,b)) return null
+            solveM_(R, b, x)
+            return x
         },
         determinant(A) {
             n = A.length
-            const m = new Array(n).fill(0).map((e, i) => [...A[i]])
+            const R = new Array(n).fill(0).map((e, i) => [...A[i]])
             let singular = false
             sign = 1
-            for (let row = 0; row < n; row++) {
-                pIndex = row
-                for (let i = row + 1; i < n; i++) { // pivot
-                    if (Math.abs(m[i][row]) > Math.abs(m[pIndex][row])) pIndex = i
+            for (let c = 0; c < n; c++) { // loop over the columns
+                pIndex = c
+                for (let i = c + 1; i < n; i++) { // pivot
+                    if (Math.abs(R[i][c]) > Math.abs(R[pIndex][c])) pIndex = i
                 }
-                if (Math.abs(m[row][pIndex]) < Number.EPSILON) {
+                if (Math.abs(R[pIndex][c]) < Number.EPSILON) {
                     singular = true // not invertible
                     break
                 }
-                if (pIndex != row) {
-                    swapRows(m, row, pIndex)
+                if (pIndex != c) {
+                    swapRows(R, c, pIndex)
                     sign *= -1
                 }
-                for (let i = row + 1; i < n; i++) {
-                    const s = m[i][row] / m[row][row]
-                    for (let j = row + 1; j < n; j++) {
-                        m[i][j] -= m[row][j] * s
+                for (let i = c + 1; i < n; i++) { // loop over rows
+                    const lic = R[i][c] / R[c][c]
+                    for (let j = c + 1; j < n; j++) { // loop over columns
+                        R[i][j] -= R[c][j] * lic
                     }
-                    m[i][row] = 0
+                    R[i][c] = 0
                 }
             }
+            if (singular) return null
             let d = 1
             for (let i = 0; i < n; i++) {
-                d *= m[i][i]
+                d *= R[i][i]
             }
             return d * sign
         }
     }
 } // gaussEliminationFactory()
+
--- common/luDecomposition.js
+++ common/luDecomposition.js
@@ -1,83 +1,65 @@
-/* LU decomposition
- * Original Implementation: https://github.com/KatIsHere/Approximation/blob/master/Linear_equations_solve.cpp
- * from the project Approximation (https://github.com/KatIsHere/Approximation)
+/**
+ * LU decomposition via Gauss Elimination
+ * The implementation is based on the text book 
+ * Hans Rudolf Schwarz I Norbert Köckler 
+ * Numerische Mathematik 
+ * 7., überarbeitete Auflage, 2009
  */
-
 export default function luDecompositionFactory() {
-    let n = -1
-    let m = null // system matrix
-    let p = null // pivot matrix
-    let s = 1 // sign to account for rows swap
-
+    let n = 0      // matrix size
+    let pIndex = 0 // pivot index
+    let R = undefined
+    let P = undefined
     const swapRows = (A, i, j) => [A[i], A[j]] = [A[j], A[i]]
-    const copy = a => {
-        const n = a.length
-        const b = new Array(n).fill(null)
-        for (let i = 0; i < n; i++) {
-            b[i] = [...a[i]]
-        }
-        return b
-    }
-    return {
-        lu(a) {
-            n = a.length
-            m = new Array(a.length).fill(null).map((e, i) => [...a[i]])
-            p = Array.from({ length: n }, (item, index) => index)
-            for (let i = 0; i < n; i++) {
-                let pValue = 0
-                let pIndex = i
-                for (let k = i; k < n; k++) {
-                    if (Math.abs(m[k][i]) > pValue) {
-                        pValue = Math.abs(m[k][i])
-                        pIndex = k
-                    }
-                }
-                if (pValue < Number.EPSILON) return false // not invertible
-                if (pIndex != i) { // pivoting
-                    [p[i],p[pIndex]] = [p[pIndex],p[i]]
-                    swapRows(m, i, pIndex)
-                    s *= -1
-                }
-                for (let j = i + 1; j < n; j++) {
-                    m[j][i] /= m[i][i]
-                    for (let k = i + 1; k < n; k++) {
-                        m[j][k] -= m[j][i] * m[i][k]
-                    }
-                }
+    function lu_(R) {
+        for (let c = 0; c < n; c++) { // loop over the columns
+            pIndex = c
+            for (let i = c + 1; i < n; i++) { // pivot
+                if (Math.abs(R[i][c]) > Math.abs(R[pIndex][c])) pIndex = i
             }
-            return true
+            if (Math.abs(R[pIndex][c]) < Number.EPSILON) return false // not invertible
+            if (pIndex != c) {
+                swapRows(R, c, pIndex)
+                swapRows(P, c, pIndex)
+            }
+            for (let i = c + 1; i < n; i++) { // loop over rows
+                const lic = R[i][c] / R[c][c]
+                for (let j = c + 1; j < n; j++) { // loop over columns
+                    R[i][j] -= R[c][j] * lic
+                }
+                R[i][c] = lic
+            }
+        }
+        return true
+    } // LU decomposition
+    function solve_(b, x) {
+        for (let i = 0; i < n; i++) {
+            x[i] = b[P[i]]
+            for (let k = 0; k < i; k++) {
+                x[i] -= R[i][k] * x[k]
+            }
+        }
+        for (let i = n - 1; i >= 0; i--) {
+            for (let k = i + 1; k < n; k++) {
+                x[i] -= R[i][k] * x[k]
+            }
+            x[i] /= R[i][i]
+        }
+        return true
+    } // solve()
+    return {
+        lu(A) {
+            n = A.length
+            R = new Array(n).fill(0).map((e, i) => [...A[i]])
+            P = [...Array(n).keys()]
+            return lu_(R)
         },
         solve(b) {
             const x = new Array(n).fill(0)
-            for (let i = 0; i < n; i++) {
-                x[i] = b[p[i]]
-                for (let k = 0; k < i; k++) {
-                    x[i] -= m[i][k] * x[k]
-                }
-            }
-            for (let i = n - 1; i >= 0; i--) {
-                for (let k = i + 1; k < n; k++) {
-                    x[i] -= m[i][k] * x[k]
-                }
-                x[i] /= m[i][i]
-            }
-            return x
-        },
-        inverse(a) {
-            const m = copy(a)
-            this.lu(m)
-            const n = m.length
-            const aInv = new Array(n).fill(0).map( e => new Array(n).fill(0))
-            const b = new Array(n).fill(0)
-            for (let i = 0; i < n; i++) {
-                b.fill(0)
-                b[i] = 1
-                const x = this.solve(b)
-                x.forEach((e,j) => {
-                    aInv[j][i] = e
-                })
-            }
-            return aInv
+            const flag = solve_(b, x)
+            if (flag) return x
+            else return null
         }
     }
-}
+} // luDecompositionFactory()
+
